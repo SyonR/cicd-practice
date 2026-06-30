db7f91711c38cc68ab76d0368746dd3bbb916d94
SyonR <syon@vt.edu>
2026-06-30 10:58:24 -0500

    modernized the application to prevent versioning issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,10 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
+@RestController
 @SpringBootApplication
 public class App {
 
@@ -21,6 +24,7 @@ public class App {
         log.info("Java app started");
     }
 
+    @GetMapping("/status")
     public String getStatus() {
         return "OK";
     }
